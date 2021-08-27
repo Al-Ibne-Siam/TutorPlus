b0237e88c61c2cac57779c83d893f9129b830978
Al-Ibne-Siam <alibnesiam@github.com>
2021-08-27 16:33:33 +0600

    index and sign-up fixed

--- js/signup.js
+++ js/signup.js
@@ -12,7 +12,7 @@ var registerform = document.getElementById('registerform');
 function validRegistration(){
 
   if(usertype.value=="1"){
-    registerform.action = "tutor.html";
+    registerform.action = "instructor.html";
   } else registerform.action = "student.html";
 
   if(fname.value==''||fname.value==null){
