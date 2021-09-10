f8402163f3837287610b9b87c9080123b973921a
Al-Ibne-Siam <alibnesiam@github.com>
2021-09-10 21:32:01 +0600

    modified tutor page

--- tutor.php
+++ tutor.php
@@ -150,7 +150,6 @@ Purchase:
 								<!-- Nav tabs -->
 								<ul class="nav nav-tabs" role="tablist">
 									<li role="presentation" class="active"><a href="#my" aria-controls="my" role="tab" data-toggle="tab">courses</a></li>
-									<li role="presentation"><a href="#instructing" aria-controls="instructing" role="tab" data-toggle="tab">conversations</a></li>
 								</ul>
 
 								<!-- Tab panes -->
@@ -165,157 +164,18 @@ Purchase:
 															<img src="http://placehold.it/248X156" alt="item1" class="img-responsive">
 														</div>
 														<div class="ed_item_description ed_most_recomended_data">
-																<h4><a href="course_single.html">Project Learning </a><span>৳25</span></h4>
-																<div class="row">
-																	<div class="ed_rating">
-																		<div class="col-lg-6 col-md-7 col-sm-6 col-xs-6">
-																			<div class="row">
-																				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-																					<div class="ed_stardiv">
-																						<div class="star-rating"><span style="width:80%;"></span></div>
-																					</div>
-																				</div>
-																				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-																					<div class="row">
-																						<p>(5 review)</p>
-																					</div>
-																				</div>
-																			</div>
-																		</div>
-																		<div class="col-lg-6 col-md-5 col-sm-6 col-xs-6">
-																			<div class="ed_views">
-																			<i class="fa fa-users"></i>
-																			<span>35 students</span>
-																			</div>
-																		</div>
-																	</div>
-																</div>
-																<p>Project-Based Learning is a flexible tool for framing given academic standards into flexible tool for framing.</p>
-																<a href="course_single.html" class="ed_getinvolved">get involved <i class="fa fa-long-arrow-right"></i></a>
-														</div>
-													</div>
-													<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 ed_bottompadder20">
-														<div class="ed_item_img">
-															<img src="http://placehold.it/248X156" alt="item1" class="img-responsive">
-														</div>
-														<div class="ed_item_description ed_most_recomended_data">
-																<h4><a href="course_single.html">Billing Seminar</a><span>৳20</span></h4>
-																<div class="row">
-																	<div class="ed_rating">
-																		<div class="col-lg-6 col-md-7 col-sm-6 col-xs-6">
-																			<div class="row">
-																				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-																					<div class="ed_stardiv">
-																						<div class="star-rating"><span style="width:80%;"></span></div>
-																					</div>
-																				</div>
-																				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-																					<div class="row">
-																						<p>(3 review)</p>
-																					</div>
-																				</div>
-																			</div>
-																		</div>
-																		<div class="col-lg-6 col-md-5 col-sm-6 col-xs-6">
-																			<div class="ed_views">
-																			<i class="fa fa-users"></i>
-																			<span>55 students</span>
-																			</div>
-																		</div>
-																	</div>
-																</div>
-																<p>We offer these billing seminars to AMA member physicians along with their staff (at least must attend the seminar).</p>
-																<a href="course_single.html" class="ed_getinvolved">get involved <i class="fa fa-long-arrow-right"></i></a>
-														</div>
-													</div>
-													<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 ed_bottompadder20">
-														<div class="ed_item_img">
-															<img src="http://placehold.it/248X156" alt="item1" class="img-responsive">
-														</div>
-														<div class="ed_item_description ed_most_recomended_data">
-																<h4><a href="course_single.html">Girls On Rails </a><span>৳40</span></h4>
-																<div class="row">
-																	<div class="ed_rating">
-																		<div class="col-lg-6 col-md-7 col-sm-6 col-xs-6">
-																			<div class="row">
-																				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-																					<div class="ed_stardiv">
-																						<div class="star-rating"><span style="width:80%;"></span></div>
-																					</div>
-																				</div>
-																				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-																					<div class="row">
-																						<p>(8 review)</p>
-																					</div>
-																				</div>
-																			</div>
-																		</div>
-																		<div class="col-lg-6 col-md-5 col-sm-6 col-xs-6">
-																			<div class="ed_views">
-																			<i class="fa fa-users"></i>
-																			<span>45 students</span>
-																			</div>
-																		</div>
-																	</div>
-																</div>
-																<p>In a lot of various jobs, psychology tests are something you will have to deal with. We can prepare you.</p>
-																<a href="course_single.html" class="ed_getinvolved">get involved <i class="fa fa-long-arrow-right"></i></a>
-														</div>
-													</div>
-													<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 ed_bottompadder20">
-														<div class="ed_item_img">
-															<img src="http://placehold.it/248X156" alt="item1" class="img-responsive">
-														</div>
-														<div class="ed_item_description ed_most_recomended_data">
-																<h4><a href="course_single.html">programming </a><span>৳38</span></h4>
-																<div class="row">
-																	<div class="ed_rating">
-																		<div class="col-lg-6 col-md-7 col-sm-6 col-xs-6">
-																			<div class="row">
-																				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-																					<div class="ed_stardiv">
-																						<div class="star-rating"><span style="width:80%;"></span></div>
-																					</div>
-																				</div>
-																				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-																					<div class="row">
-																						<p>(9 review)</p>
-																					</div>
-																				</div>
-																			</div>
-																		</div>
-																		<div class="col-lg-6 col-md-5 col-sm-6 col-xs-6">
-																			<div class="ed_views">
-																			<i class="fa fa-users"></i>
-																			<span>60 students</span>
-																			</div>
-																		</div>
-																	</div>
-																</div>
-																<p>Sometimes students need help with things unrelated to school. Where we come in and make sure you are happy .</p>
-																<a href="course_single.html" class="ed_getinvolved">get involved <i class="fa fa-long-arrow-right"></i></a>
+																<h4><a href="course_single.html"> Student Name</a></h4>
+                                <h5>Salary: ৳1000-৳10000</h5>
+                                <h5>Area: </h5>
+                                <h5>Institution: </h5>
+                                <h5>Curriculum: </h5>
+                                <h5>Background: </h5>
+                                <textarea disabled rows="5" cols="30">Project-Based Learning is a flexible tool for framing given academic standards into flexible tool for framing.
+                                </textarea>
+																<a href="course_single.html" class="ed_getinvolved">Interested<i class="fa fa-long-arrow-right"></i></a>
 														</div>
 													</div>
 												</div>
-											</div>
-										</div>
-									</div>
-									<div role="tabpanel" class="tab-pane" id="instructing">
-										<div class="ed_inner_dashboard_info">
-											<div class="convo-box">
-												<h4>Aniket Prantor</h4>
-												<p> >> Hello !</p>
-												<p> << What is your speciality? </p>
-
-												<form class="ed_tabpersonal">
-													<div class="form-group">
-													<textarea name="whats_new" class="form-control" id="whats_new" cols="50" rows="5"></textarea>
-													</div>
-													<div class="form-group">
-													<button class="btn ed_btn ed_green">Send Message</button>
-													</div>
-												</form>
-
 											</div>
 										</div>
 									</div>
@@ -329,9 +189,8 @@ Purchase:
 							<div role="tabpanel">
 								<!-- Nav tabs -->
 								<ul class="nav nav-tabs" role="tablist">
-									<li role="presentation" class="active"><a href="#personal" aria-controls="personal" role="tab" data-toggle="tab">personal</a></li>
-									<li role="presentation"><a href="#mentions" aria-controls="mentions" role="tab" data-toggle="tab">mentions</a></li>
-									<li role="presentation"><a href="#favourites" aria-controls="favourites" role="tab" data-toggle="tab">favourites</a></li>
+									<li role="presentation" class="active"><a href="#personal" aria-controls="personal" role="tab" data-toggle="tab">Matches</a></li>
+                  <li role="presentation"><a href="#instructing" aria-controls="instructing" role="tab" data-toggle="tab">conversations</a></li>
 								</ul>
 
 								<!-- Tab panes -->
@@ -340,55 +199,11 @@ Purchase:
 									<div class="ed_inner_dashboard_info">
 										<div class="ed_course_single_info">
 											<div class="ed_add_students">
-												<img src="http://placehold.it/50X50" alt="">
-												<span>student started course course status 1 weeks, 2 days ago</span>
-												<p>Student adler braxton started the course Course Status</p>
+												<h4>Congratulations! You have matched with Student A</h4>
+												<h6>Contact student ASAP!</h6>
+                        <textarea disabled rows="2" cols="40">Phone:01990188461 email:alibnesiam@gmail.com</textarea>
 											</div>
-											<div class="ed_add_students">
-												<img src="http://placehold.it/50X50" alt="">
-												<span>student started course course status 1 weeks, 4 days ago</span>
-												<p>Student baldwin dallas started the course Course Status</p>
-											</div>
-											<div class="ed_add_students">
-												<img src="http://placehold.it/50X50" alt="">
-												<span>student started course course status 1 weeks, 3 days ago</span>
-												<p>Student carney Tate started the course Course Status</p>
-											</div>
-											<div class="ed_add_students">
-												<img src="http://placehold.it/50X50" alt="">
-												<span>student started course course status 2 weeks, 4 days ago</span>
-												<p>Student dwight easton started the course Course Status</p>
-											</div>
-											<div class="ed_add_students">
-												<img src="http://placehold.it/50X50" alt="">
-												<span>student started course course status 2 weeks, 5 days ago</span>
-												<p>Student elbert wade started the course Course Status</p>
-											</div>
-											<div class="ed_add_students">
-												<img src="http://placehold.it/50X50" alt="">
-												<span>student started course course status 2 weeks, 2 days ago</span>
-												<p>Student hailey kyler started the course Course Status</p>
-											</div>
-											<div class="ed_add_students">
-												<img src="http://placehold.it/50X50" alt="">
-												<span>student started course course status 3 weeks, 3 days ago</span>
-												<p>Student graham ryder started the course Course Status</p>
-											</div>
-											<div class="ed_add_students">
-												<img src="http://placehold.it/50X50" alt="">
-												<span>student started course course status 3 weeks, 6 days ago</span>
-												<p>Student jaxon keegan started the course Course Status</p>
-											</div>
-											<div class="ed_add_students">
-												<img src="http://placehold.it/50X50" alt="">
-												<span>student started course course status 4 weeks, 2 days ago</span>
-												<p>Student kealy sage started the course Course Status</p>
-											</div>
-											<div class="ed_add_students">
-												<img src="http://placehold.it/50X50" alt="">
-												<span>student started course course status 4 weeks, 4 days ago</span>
-												<p>Student lavern gunner started the course Course Status</p>
-											</div>
+
 											<div class="col-lg-12">
 											<div class="row">
 												<div class="ed_blog_bottom_pagination ed_toppadder40">
@@ -406,17 +221,26 @@ Purchase:
 										</div>
 									</div>
 									</div>
-									<div role="tabpanel" class="tab-pane" id="mentions">
-									<div class="ed_inner_dashboard_info">
-										<h2>sorry, there was no mentions event found. please try a different filter</h2>
-									</div>
-									</div>
-									<div role="tabpanel" class="tab-pane" id="favourites">
-									<div class="ed_inner_dashboard_info">
-										<h2>sorry, there was no favourites event found. please try a different filter</h2>
-									</div>
+                  <div role="tabpanel" class="tab-pane" id="instructing">
+										<div class="ed_inner_dashboard_info">
+											<div class="convo-box">
+												<h4>Aniket Prantor</h4>
+												<p> >> Hello !</p>
+												<p> << What is your speciality? </p>
+												<form class="ed_tabpersonal">
+													<div class="form-group">
+													<textarea name="whats_new" class="form-control" id="whats_new" cols="50" rows="5"></textarea>
+													</div>
+													<div class="form-group">
+													<button class="btn ed_btn ed_green">Send Message</button>
+													</div>
+												</form>
+
+											</div>
+										</div>
 									</div>
 								</div>
+
 
 							</div><!--tab End-->
 						</div>
